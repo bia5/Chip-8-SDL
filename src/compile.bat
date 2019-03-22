f3caf4c363b25cfbb262051a5eb8c540c5f1abc0
ThePixelCoder <thepixelmaster2354@gmail.com>
2019-03-22 07:24:34 -0500

    Update compile.bat

--- src/compile.bat
+++ src/compile.bat
@@ -1,4 +1,2 @@
-g++ -o ..\chip8.exe main.cpp chip8.cpp -lmingw32 -lSDL2main -lSDL2
+g++ -o ..\chip8.exe main.cpp chip8.cpp -lmingw32 -lSDL2main -lSDL2 -static-libgcc -static-libstdc++
 pause
-
-::g++ -o main.exe main.cpp -lmingw32 -lSDL2main -lSDL2
